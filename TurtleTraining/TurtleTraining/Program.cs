9483f8837d98a28d53c2019317724a66fe8dc548
Tamozhnii <petek379@ukr.net>
2019-02-24 20:48:32 +0300

    2

--- TurtleTraining/TurtleTraining/Program.cs
+++ TurtleTraining/TurtleTraining/Program.cs
@@ -12,21 +12,27 @@ namespace TurtleTraining
         static void Main(string[] args)
         {
             Turtle.Speed = 7;
-            int x = 25;
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    Turtle.Move(x);
-                    Turtle.TurnRight();
-                }
-                for (int j = 0; j < 2; j++)
-                {
-                    Turtle.Move(x);
-                    Turtle.TurnLeft();
-                }
-            }
+            //int x = 25;
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Turtle.Move(x);
+            //        Turtle.TurnRight();
+            //    }
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Turtle.Move(x);
+            //        Turtle.TurnLeft();
+            //    }
+            //}
 
+            double x = 90 / 1.5;
+            for (int i = 0; i < 6; i++)
+            {
+                Turtle.Move(100);
+                Turtle.Turn(x);
+            }
         }
     }
 }
