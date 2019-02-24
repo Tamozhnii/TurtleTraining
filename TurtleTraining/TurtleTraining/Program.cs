cb09f1e0a24e09e4f08df24750d752a764a695b5
Tamozhnii <petek379@ukr.net>
2019-02-24 22:07:01 +0300

    1

--- TurtleTraining/TurtleTraining/Program.cs
+++ TurtleTraining/TurtleTraining/Program.cs
@@ -9,30 +9,58 @@ namespace TurtleTraining
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WriteT (int Size)
         {
-            Turtle.Speed = 7;
-            //int x = 25;
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    for (int j = 0; j < 2; j++)
-            //    {
-            //        Turtle.Move(x);
-            //        Turtle.TurnRight();
-            //    }
-            //    for (int j = 0; j < 2; j++)
-            //    {
-            //        Turtle.Move(x);
-            //        Turtle.TurnLeft();
-            //    }
-            //}
+            Turtle.Move(Size);
+            Turtle.TurnLeft();
+            Turtle.Move(Size/2);
+            Turtle.Turn(180);
+            Turtle.Move(Size);
+        }
 
-            double x = 90 / 1.5;
-            for (int i = 0; i < 6; i++)
+        static void WriteO(int c)
+        {
+            int r = 360;
+            double Move = 0.5 / c;
+            double x = 360 / r;
+            for (int i = 0; i < (r / c); i++)
             {
-                Turtle.Move(100);
+                Turtle.Move(Move);
                 Turtle.Turn(x);
             }
+        }
+
+        static void Position(int x)
+        {
+            Turtle.Angle = 0;
+            double Word = 100 * x;
+            Turtle.X = 50 + Word;
+            Turtle.Y = 200;
+        }
+
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 8;
+
+            Position(1);
+            WriteT(100);
+
+            Position(2);
+            Turtle.TurnLeft();
+            WriteO(1);
+
+            Position(3);
+            Turtle.Move(50);
+            Turtle.TurnRight();
+            WriteO(2);
+
+            Position(4);
+            WriteT(50);
+
+            double p = (450 + 150) / 2;
+            Turtle.X = p;
+            Turtle.Y = 250;
+            Turtle.Angle = 0;
         }
     }
 }
