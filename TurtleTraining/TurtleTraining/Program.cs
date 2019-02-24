8582b9d9dd4a7c2f6e2007441cf9f7d12b1092cf
Tamozhnii <petek379@ukr.net>
2019-02-24 20:30:35 +0300

    Lesson1

--- TurtleTraining/TurtleTraining/Program.cs
+++ TurtleTraining/TurtleTraining/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
 
 namespace TurtleTraining
 {
@@ -10,6 +11,13 @@ namespace TurtleTraining
     {
         static void Main(string[] args)
         {
+            Turtle.Speed = 9;
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.Move(100);
+                Turtle.TurnRight();
+            }
+            
         }
     }
 }
